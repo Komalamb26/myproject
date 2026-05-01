6e18277479ef2bf6a44dc325ebd3b998e246e430
Komala <komalamatth.is23@bmsce.ac.in>
2026-05-01 21:24:06 +0530

    Added Flask REST web service

---
+++ webserver.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Sample data
+books = [
+    {"id": 1, "title": "Book 1", "author": "Author 1"},
+    {"id": 2, "title": "Book 2", "author": "Author 2"},
+    {"id": 3, "title": "Book 3", "author": "Author 3"}
+]
+
+# GET all books
+@app.route('/books', methods=['GET'])
+def get_books():
+    return jsonify(books)
+
+# GET book by id
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    for book in books:
+        if book["id"] == id:
+            return jsonify(book)
+    return jsonify({"error": "Book not found"}), 404
+
+# POST add new book
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.json
+
+    new_book = {
+        "id": len(books) + 1,
+        "title": data["title"],
+        "author": data["author"]
+    }
+
+    books.append(new_book)
+    return jsonify(new_book), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
